8a5f65338c13629acf2a6628386c4db4c1776c31
Finley Terlouw <88457210+devdxniel@users.noreply.github.com>
2022-01-02 21:31:32 +0000

    Encoded SpidermanTrailer

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("RatatouilleEnding","RatatouilleEndingVideo.mp4",.1,.1,10)
+    encoder.encode("SpidermanTrailer","SPIDER-MAN_ NO WAY HOME - Official Trailer (HD).mp4",.1,.1,10)
 
 if __name__== "__main__":
     main()
